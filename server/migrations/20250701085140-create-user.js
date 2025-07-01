babde2743bc8ad972d800c06c39c4ddac5b3ceb8
hartashu <harta.shuwanto@gmail.com>
2025-07-01 19:06:33 +0700

    feat: Add socket logic

--- server/migrations/20250701085140-create-user.js
+++ server/migrations/20250701085140-create-user.js
@@ -24,7 +24,7 @@ module.exports = {
         allowNull: false,
       },
       imageUrl: {
-        type: Sequelize.STRING,
+        type: Sequelize.TEXT,
         allowNull: false,
       },
       cpm: {
